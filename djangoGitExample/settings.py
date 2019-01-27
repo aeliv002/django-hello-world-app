f191536db264b5bda9758857cc9f316095030a92
Kristijonas <k.impolevicius@gmail.com>
2019-01-27 23:19:53 +0200

    allowwed host changed

--- djangoGitExample/settings.py
+++ djangoGitExample/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8k^46aiz1#^s$2o&#cahk@@*jn1y@p$_k@fn$vst*oje3k2*n2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
